60d33a80acab58ff65c9299a67589f7b304e6471
Facu-Vaz <facundo.vazber@gmail.com>
2023-02-26 11:59:56 -0300

    feat(CSES)

---
+++ CSES/Graphs/Labyrinth.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+    int H, W, start, end;
+    cin >> H >> W;
+    int num_nodes = 1;
+    vector<vector<int>> adj;
+    vector<vector<bool>> not_wall (H, vector<bool> (W, false));
+    char temp;
+    for (int h = 0; h < H; h++)
+    {
+        for (int w = 0; w < W; w++)
+        {
+            cin >> temp;
+            if (temp != '#') {
+                icon 
+            }
+        }
+    }
+}
